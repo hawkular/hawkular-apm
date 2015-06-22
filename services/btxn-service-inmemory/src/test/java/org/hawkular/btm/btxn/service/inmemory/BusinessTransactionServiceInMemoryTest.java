7a2fa4968641b837cb8ab5af2706fcd9a47771fc
Gary Brown <gary@brownuk.com>
2015-06-22 17:25:53 +0100

    HWKBTM-41 Add in-memory implementation of the btxn service, to support testing and non-persistent environments

---
+++ services/btxn-service-inmemory/src/test/java/org/hawkular/btm/btxn/service/inmemory/BusinessTransactionServiceInMemoryTest.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2015 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.hawkular.btm.btxn.service.inmemory;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.fail;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hawkular.btm.api.model.btxn.BusinessTransaction;
+import org.hawkular.btm.api.model.btxn.Consumer;
+import org.hawkular.btm.api.model.btxn.Service;
+import org.junit.Test;
+
+/**
+ * @author gbrown
+ */
+public class BusinessTransactionServiceInMemoryTest {
+
+    @Test
+    public void testStoreMax() {
+        BusinessTransactionServiceInMemory.setMaxTransactions(3);
+
+        BusinessTransactionServiceInMemory bts=new BusinessTransactionServiceInMemory();
+
+        try {
+            bts.store(null, createTransactions(1));
+            bts.store(null, createTransactions(11));
+        } catch (Exception e) {
+            fail("Failed to store txns: "+e);
+        }
+
+        // Check only 3 txns left
+        assertEquals(3, BusinessTransactionServiceInMemory.getTxns().size());
+
+        assertNotNull(bts.get(null, "2"));
+        assertNotNull(bts.get(null, "11"));
+        assertNotNull(bts.get(null, "12"));
+
+        assertNull(bts.get(null, "1"));
+    }
+
+    protected static List<BusinessTransaction> createTransactions(int baseid) {
+        BusinessTransaction btxn1 = new BusinessTransaction();
+        btxn1.setId(""+baseid);
+
+        Consumer node1_1 = new Consumer();
+        node1_1.setDuration(10000);
+        node1_1.setStartTime(100);
+        btxn1.getNodes().add(node1_1);
+
+        Service node1_2 = new Service();
+        node1_2.setDuration(9000);
+        node1_2.setStartTime(150);
+        node1_1.getNodes().add(node1_2);
+
+        btxn1.getNodes().add(node1_1);
+
+        BusinessTransaction btxn2 = new BusinessTransaction();
+        btxn2.setId(""+(baseid+1));
+
+        Consumer node2_1 = new Consumer();
+        node2_1.setDuration(10000);
+        node2_1.setStartTime(100);
+        btxn2.getNodes().add(node2_1);
+
+        Service node2_2 = new Service();
+        node2_2.setDuration(9000);
+        node2_2.setStartTime(150);
+        node2_1.getNodes().add(node2_2);
+
+        btxn1.getNodes().add(node2_1);
+
+        List<BusinessTransaction> btxns = new ArrayList<BusinessTransaction>();
+        btxns.add(btxn1);
+        btxns.add(btxn2);
+
+        return btxns;
+    }
+
+}
