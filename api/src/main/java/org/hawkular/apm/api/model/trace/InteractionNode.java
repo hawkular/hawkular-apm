80fd0c693bd7e0dbba16263e15d571295f9f47a1
Artyom Melnikov <artyom.melnikov@devfactory.com>
2016-06-21 10:21:27 +0300

    Fixing squid:S2259 - Null pointers should not be dereferenced

--- api/src/main/java/org/hawkular/apm/api/model/trace/InteractionNode.java
+++ api/src/main/java/org/hawkular/apm/api/model/trace/InteractionNode.java
@@ -118,6 +118,9 @@ public abstract class InteractionNode extends ContainerNode {
     public boolean equals(Object obj) {
         if (this == obj)
             return true;
+        if (obj == null) {
+            return false;
+        }
         if (!super.equals(obj))
             return false;
         if (getClass() != obj.getClass())
