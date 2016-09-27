410bc22ee262660a4bc964a3205d99c37752e64b
Gary Brown <gary@brownuk.com>
2016-09-27 12:17:39 +0100

    HWKAPM-626 Test to ensure that jackson is shaded within the agent

---
+++ client/agent/src/test/java/org/hawkular/apm/client/agent/ShadeTest.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2015-2016 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.hawkular.apm.client.agent;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+/**
+ * @author gbrown
+ */
+public class ShadeTest {
+
+    @Test
+    public void testAgentFound() {
+        try {
+            Class.forName("org.hawkular.apm.instrumenter.APMAgent");
+        } catch (ClassNotFoundException e) {
+            fail("Class should have been found");
+        }
+    }
+
+    @Test(expected=ClassNotFoundException.class)
+    public void testJacksonNotFound() throws ClassNotFoundException {
+        Class.forName("com.fasterxml.jackson.databind.ObjectMapper");
+    }
+
+}
