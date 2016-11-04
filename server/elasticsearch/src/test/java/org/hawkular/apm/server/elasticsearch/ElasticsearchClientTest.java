eaac14365c6a85904bcec940ad456aa059fae4c2
Gary Brown <gary@brownuk.com>
2016-11-04 16:39:08 +0000

    HWKAPM-724 Fix issue where empty string for Elasticsearch hosts property causes error

---
+++ server/elasticsearch/src/test/java/org/hawkular/apm/server/elasticsearch/ElasticsearchClientTest.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2015-2016 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.hawkular.apm.server.elasticsearch;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author gbrown
+ */
+public class ElasticsearchClientTest {
+
+    private static final String TESTHOSTS = "testhosts";
+
+    @Test
+    public void testDefaultElasticsearchHostNull() {
+        System.clearProperty(ElasticsearchClient.ELASTICSEARCH_HOSTS);
+        ElasticsearchClient client = new ElasticsearchClient();
+        assertEquals(ElasticsearchClient.ELASTICSEARCH_HOSTS_DEFAULT, client.getHosts());
+    }
+
+    @Test
+    public void testDefaultElasticsearchHostEmptyString() {
+        System.setProperty(ElasticsearchClient.ELASTICSEARCH_HOSTS, "");
+        ElasticsearchClient client = new ElasticsearchClient();
+        assertEquals(ElasticsearchClient.ELASTICSEARCH_HOSTS_DEFAULT, client.getHosts());
+    }
+
+    @Test
+    public void testDefaultElasticsearchHostValue() {
+        System.setProperty(ElasticsearchClient.ELASTICSEARCH_HOSTS, TESTHOSTS);
+        ElasticsearchClient client = new ElasticsearchClient();
+        assertEquals(TESTHOSTS, client.getHosts());
+    }
+
+}
