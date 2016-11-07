0035d2563e3219e0354d79700f0ce8b67ed4a961
Alexandre Mendonça <amendonca@redhat.com>
2016-11-07 19:38:48 +0000

    HWKAPM-602 : Create hkTime filter for durations
    Add an angular filter to convert from μs duration to readable format.

--- ui/src/main/scripts/plugins/e2e/ts/e2ePlugin.ts
+++ ui/src/main/scripts/plugins/e2e/ts/e2ePlugin.ts
@@ -26,5 +26,44 @@ module E2E {
     // Doesn't link an empty block
   }]);
 
+  _module.filter('hkTime', function (limitToFilter, numberFilter) {
+    let milliRange =  1000;
+    let secondRange = 1000 * milliRange;
+    let minuteRange =   60 * secondRange;
+    let hourRange =     60 * minuteRange;
+    let dayRange =      24 * hourRange;
+    let weekRange =      7 * dayRange;
+    let yearRange =    365 * dayRange;
+
+    return function (input) {
+      if (input && !isNaN(input)) {
+        if (input > yearRange) {
+          let weeks = input / weekRange;
+          return Math.floor(weeks / 52) + 'y ' + numberFilter(Math.floor(weeks % 52), 0) + 'w';
+        } else if (input > weekRange) {
+          let days = input / dayRange;
+          return Math.floor(days / 7) + 'w ' + numberFilter(Math.floor(days % 7), 0) + 'd';
+        } else if (input > dayRange) {
+          let hours = input / hourRange;
+          return Math.floor(hours / 24) + 'd ' + numberFilter(Math.floor(hours % 24), 0) + 'h';
+        } else if (input > hourRange) {
+          let minutes = input / minuteRange;
+          return Math.floor(minutes / 60) + 'h ' + numberFilter(Math.floor(minutes % 60), 0) + 'min';
+        } else if (input > minuteRange) {
+          let seconds = input / secondRange;
+          return Math.floor(seconds / 60) + 'min ' + numberFilter(Math.floor(seconds % 60), 0) + 's';
+        } else if (input > secondRange) {
+          return Math.floor(input / secondRange) + 's ' + limitToFilter(Math.floor(input % secondRange), 3) + 'ms';
+        } else if (input > milliRange) {
+          return Math.floor(input / milliRange) + 'ms ' + limitToFilter(Math.floor(input % milliRange), 3) + 'μs';
+        } else {
+          return input + 'μs';
+        }
+      } else {
+        return input;
+      }
+    };
+  });
+
   hawtioPluginLoader.addModule(E2E.pluginName);
 }
