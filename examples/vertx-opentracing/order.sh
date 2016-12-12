4747f94739d8846649af104ca301eadc898f8675
Gary Brown <gary@brownuk.com>
2016-12-12 15:38:55 +0000

    HWKAPM-784 Updated trace instance diagram and fixed scripts to point to the correct port

--- examples/vertx-opentracing/order.sh
+++ examples/vertx-opentracing/order.sh
@@ -20,7 +20,7 @@ echo "Creating an order for account=$1 item=$2 quantity=$3"
 
 data="{\"accountId\":\"$1\",\"itemId\":\"$2\",\"quantity\": $3}"
 
-curl -X POST -H "Content-Type: application/json" -d "$data" http://localhost:8080/orders
+curl -X POST -H "Content-Type: application/json" -d "$data" http://localhost:8180/orders
 
 echo
 echo
